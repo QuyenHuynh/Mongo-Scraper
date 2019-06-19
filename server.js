8b0c3470cdfc87b1f8800144512720664707c346
Quyen Huynh <quyenh@hotmail.com>
2019-06-19 18:47:25 -0400

    successful scrape

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const exphbs = require("express-handlebars");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-// Scraping tools
 const axios = require('axios');
 const cheerio = require('cheerio');
+
 // Require all models
 const db = require("./models");
 
@@ -13,11 +13,10 @@ const db = require("./models");
 const app = express();
 
 // Set the port of our application
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-// Use morgan logger for logging requests
+// Middleware 
 app.use(logger("dev"));
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -27,25 +26,54 @@ app.set("view engine", "handlebars");
 
 // Connect to Mongo
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 //Routes
 
 // GET route for homepage
-app.get("/", function(req, res) {
-  res.render(index);
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
 // GET route for scraped data from dev.to
 app.get('/scrape', function (req, res) {
   axios.get("https://dev.to").then(function (response) {
-    console.log(response.data);
+    // console.log(response.data);
     const $ = cheerio.load(response.data);
 
-  })
-});
+    // Now, we grab every h2 within an article tag, and do the following:
+    $(".single-article").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
 
-// GET route for all articles in the db
+      // Add the title, text, and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a").children("div").children("h3")
+        .text().trim();
+      result.link = "https://dev.to" + $(this)
+        .children(".index-article-link")
+        .attr("href");
+      result.author = $(this)
+        .children("h4")
+        .text().trim();
+
+      console.log(result);
+
+      // Create Article object in the database using results
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+
+      res.render("index", result);
+    })
+  });
+})
+
+// GET route for all saved articles in the db
 app.get('/articles', function (req, res) {
   db.Article.find({})
     .then(function (dbArticle) {
@@ -82,28 +110,40 @@ app.post("/articles/:id", function (req, res) {
     });
 });
 
-// PUT Route for saving/updating article to be saved
-app.put("/saved/:id", function(req, res) {
+
+// GET Route for getting saved article
+app.get("/saved", function (req, res) {
 
   db.Article
-    .findByIdAndUpdate({ _id: req.params.id }, { $set: { isSaved: true }})
-    .then(function(dbArticle) {
+    .find({ isSaved: true })
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-// GET Route for getting saved article
-app.get("/saved", function(req, res) {
-
+// PUT route for deleting/updating saved article
+app.put("/delete/:id", function (req, res) {
   db.Article
-    .find({ isSaved: true })
-    .then(function(dbArticle) {
+    .findByIdAndUpdate({ _id: req.params.id }, { $set: { isSaved: false } })
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+// PUT Route for saving/updating article to be saved
+app.put("/saved/:id", function (req, res) {
+  db.Article
+    .findByIdAndUpdate({ _id: req.params.id }, { $set: { isSaved: true } })
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
       res.json(err);
     });
 });
@@ -124,7 +164,7 @@ app.post('/createNote/:id', function (req, res) {
     });
 });
 
-//get route to retrieve all notes for a particlular article
+//GET route to retrieve all notes for a particlular article
 app.get('/getNotes/:id', function (req, res) {
   db.Article
     .findOne({ _id: req.params.id })
@@ -136,6 +176,7 @@ app.get('/getNotes/:id', function (req, res) {
       res.json(err);
     });
 });
+
 
 //start the server
 app.listen(PORT, function () {
